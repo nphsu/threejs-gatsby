fd01d8da9eff354cd3fe7b4401a66893c4359bba
shunp <shunpei42ba@gmail.com>
2020-03-21 21:34:16 +0800

    add simple scene

---
+++ src/components/three/SimpleScene.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, createRef } from 'react'
+import { css } from '@emotion/core'
+import * as THREE from 'three'
+
+const createDefaultCamera = () => {
+  const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000)
+  camera.position.z = 5
+  return camera
+}
+
+const createDefaultRenderer = (mount: React.RefObject<HTMLInputElement>) => {
+  const renderer = new THREE.WebGLRenderer({ antialias: true })
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.autoClear = true
+  if (mount.current) {
+    mount.current.appendChild(renderer.domElement)
+  }
+  return renderer
+}
+
+const ObjLoader2Scene = () => {
+  const mount = createRef<HTMLInputElement>()
+  useEffect(() => {
+    const scene = new THREE.Scene()
+    const camera = createDefaultCamera()
+    const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
+    const material = new THREE.MeshNormalMaterial()
+    const mesh = new THREE.Mesh(geometry, material)
+    scene.add(mesh)
+    const renderer = createDefaultRenderer(mount)
+    renderer.render(scene, camera)
+  }, [])
+  return <div css={css``} ref={mount} />
+}
+export default ObjLoader2Scene
