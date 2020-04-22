73b15b88bd6ae49a52f8928a6401d7ad779386b9
shunp <shunpei42ba@gmail.com>
2020-04-22 21:32:51 +0800

    delete to fix bug

--- src/pages/attribute-point.tsx
+++ src/pages/attribute-point.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 import GalleryBoxes from '../views/GalleryBoxes'
-import AttributePointScene from '../components/three/AttributePointScene'
+// import AttributePointScene from '../components/three/AttributePointScene'
 
 export default () => (
   <>
     <GalleryBoxes />
-    <AttributePointScene />
+    {/* <AttributePointScene /> */}
   </>
 )
