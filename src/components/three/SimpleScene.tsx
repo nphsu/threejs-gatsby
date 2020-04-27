02246ab8e73a7fad94b5c50c8134eb41de639d2d
shunp <shunpei42ba@gmail.com>
2020-04-27 19:29:19 +0800

    change simple scene

--- src/components/three/SimpleScene.tsx
+++ src/components/three/SimpleScene.tsx
@@ -2,13 +2,13 @@ import React, { useEffect, createRef } from 'react'
 import { css } from '@emotion/core'
 import * as THREE from 'three'
 
-const createDefaultCamera = () => {
+const newCamera = () => {
   const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000)
   camera.position.z = 5
   return camera
 }
 
-const createDefaultRenderer = (mount: React.RefObject<HTMLInputElement>) => {
+const newRenderer = (mount: React.RefObject<HTMLInputElement>) => {
   const renderer = new THREE.WebGLRenderer({ antialias: true })
   renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setSize(window.innerWidth, window.innerHeight)
@@ -19,17 +19,31 @@ const createDefaultRenderer = (mount: React.RefObject<HTMLInputElement>) => {
   return renderer
 }
 
+const newScene = () => {
+  const scene = new THREE.Scene()
+  return scene
+}
+
 const SimpleScene = () => {
   const mount = createRef<HTMLInputElement>()
   useEffect(() => {
-    const scene = new THREE.Scene()
-    const camera = createDefaultCamera()
+    const scene = newScene()
+    const camera = newCamera()
+    const renderer = newRenderer(mount)
+
     const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
     const material = new THREE.MeshNormalMaterial()
     const mesh = new THREE.Mesh(geometry, material)
     scene.add(mesh)
-    const renderer = createDefaultRenderer(mount)
-    renderer.render(scene, camera)
+
+    const render = () => {
+      renderer.render(scene, camera)
+    }
+    const animate = () => {
+      requestAnimationFrame(animate)
+      render()
+    }
+    animate()
   }, [])
   return <div css={css``} ref={mount} />
 }
